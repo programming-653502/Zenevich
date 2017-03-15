22194257289b0839ecdc532cf6ac32d917e24486
Zinya <zenevich-aleksej@mail.ru>
2017-03-16 00:21:31 +0300

    Added 1st Lab

---
+++ 1.2(12).cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	unsigned int m, n = 1, t = 1;
+	char b[50] = "", a[50] = "";
+	bool c = true;
+
+	for (; c;)
+	{
+		c = false;
+		cin >> a;
+
+		for (int i = 0; i < strlen(a); i++)
+			if (a[i]<'0' || a[i]>'9')
+			{
+				c = true;
+				break;
+			}
+
+		if (!c) m = atoi(a);
+		if (m == 0) c = true;
+	}
+
+	for (;;)
+	{
+
+		t *= 2;
+		n = t - 1;
+		if (n > m) break;
+		_itoa_s(n, b, 2);
+		cout << n << "\t" << b << endl;
+	}
+
+	system("pause");
+	return 0;
+}
